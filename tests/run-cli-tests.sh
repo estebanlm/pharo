a34a1306e3c84a7d764323edf2af75dd1e162e19
Christophe Demarey <christophe.demarey@inria.fr>
2025-08-22 14:41:31 +0200

    run all cli tests

--- tests/run-cli-tests.sh
+++ tests/run-cli-tests.sh
@@ -70,5 +70,5 @@ export ROOT_DIR
 export PHARO
 export IMAGE
 echo ">> Running tests with bats-core"
-"$BATS_DIR/bin/bats" --report-formatter junit pharo-cli-perform.bats #"$ROOT_DIR"
+"$BATS_DIR/bin/bats" --report-formatter junit "$ROOT_DIR"
 
