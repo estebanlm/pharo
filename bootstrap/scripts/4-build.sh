c6c99c8528db243203150014f9ee115f4dbe9861
CyrilFerlicot <cyril@ferlicot.fr>
2025-12-24 19:33:31 +0100

    Remove now useless initialization file

--- bootstrap/scripts/4-build.sh
+++ bootstrap/scripts/4-build.sh
@@ -181,7 +181,6 @@ ${VM} "${MC_BOOTSTRAP_IMAGE_NAME}.image" save ${METACELLO_IMAGE_NAME}
 echo "Loading packages: $(cat hermesMetacelloPackages.txt)"
 ${VM} "${MC_BOOTSTRAP_IMAGE_NAME}.image" loadHermes $(cat hermesMetacelloPackages.txt) --save --no-fail-on-undeclared
 ${VM} "${MC_BOOTSTRAP_IMAGE_NAME}.image" st ${BOOTSTRAP_REPOSITORY}/bootstrap/scripts/02-monticello-bootstrap/01-bootstrapMonticello.st --save --quit
-${VM} "${METACELLO_IMAGE_NAME}.image" st ${BOOTSTRAP_REPOSITORY}/bootstrap/scripts/03-metacello-bootstrap/01-loadMetacello.st --save --quit
 ${VM} "${METACELLO_IMAGE_NAME}.image" perform --save PharoBootstrapInitialization enableChangeLog # From now on the change log will be registered.
 git clone https://github.com/pharo-vcs/tonel.git -b "Pharo${PHARO_MAJOR}" "${BOOTSTRAP_CACHE}/tonel"
 ${VM} "${METACELLO_IMAGE_NAME}.image" metacello install --save --signalErrorOnWarning "filetree://${BOOTSTRAP_CACHE}/tonel" Tonel --groups core
